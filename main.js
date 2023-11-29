79d673394e05847903914c053731c6d3de1b2853
Zay72 <145041054+Zay72@users.noreply.github.com>
2023-11-29 09:40:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,67 +2,70 @@
 // Add Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
 // Define Variables
-let in1 = document.getElementById("in-1");
-let in2 = document.getElementById("in-2");
-let in3 = document.getElementById("in-3");
-let in4 = document.getElementById("in-4");
+let answer1 = document.getElementById("in-1");
+let answer2 = document.getElementById("in-2");
+let answer3 = document.getElementById("in-3");
+let answer4 = document.getElementById("in-4");
 let out1 = document.getElementById("out");
 let out2 = document.getElementById("out-2");
 let out3 = document.getElementById("out-3");
 let out4 = document.getElementById("out-4");
-let total = document.getElementById("tot");
+let grade = document.getElementById("final-grade");
 // If Statements
 function btnClicked() {
   i = 0;
-  if (in1.value.toLowerCase() === "july") {
+  if (answer1.value.toLowerCase() === "july") {
     out1.innerHTML = "CORRECT";
     out1.style.color = "green";
     i++;
-    in1.style.borderColor = "lime";
+    answer1.style.borderColor = "lime";
   } else {
     out1.innerHTML = "INCORRECT";
     out1.style.color = "red";
-    in1.style.borderColor = "red";
+    answer1.style.borderColor = "red";
   }
-  if (in2.value.toLowerCase() === "minecraft") {
+  if (answer2.value.toLowerCase() === "minecraft") {
     out2.innerHTML = "CORRECT";
     out2.style.color = "green";
     i++;
-    in2.style.borderColor = "lime";
+    answer2.style.borderColor = "lime";
   } else {
     out2.innerHTML = "INCORRECT";
     out2.style.color = "red";
-    in2.style.borderColor = "red";
+    answer2.style.borderColor = "red";
   }
   if (
-    in3.value.toLowerCase() === "soccer" ||
-    in3.value.toLowerCase() === "football"
+    answer3.value.toLowerCase() === "soccer" ||
+    answer3.value.toLowerCase() === "football"
   ) {
     out3.innerHTML = "CORRECT";
     out3.style.color = "green";
     i++;
-    in3.style.borderColor = "lime";
+    answer3.style.borderColor = "lime";
   } else {
     out3.innerHTML = "INCORRECT";
     out3.style.color = "red";
-    in3.style.borderColor = "red";
+    answer3.style.borderColor = "red";
   }
-  if (in4.value.toLowerCase() === "chicken") {
+  if (answer4.value.toLowerCase() === "chicken") {
     out4.innerHTML = "CORRECT";
     out4.style.color = "green";
     i++;
-    in4.style.borderColor = "lime";
+    answer4.style.borderColor = "lime";
   } else {
     out4.innerHTML = "INCORRECT";
     out4.style.color = "red";
-    in4.style.borderColor = "red";
+    answer4.style.borderColor = "red";
   }
-  let percent = (i * 100) / 4;
 
-  total.innerHTML = `${i}/4 or ${percent}%`;
+  // Mark Quiz
+  let percent = (i * 100) / 4;
+  grade.innerHTML = `${i}/4 or ${percent}%`;
+
+  // Encouragement and Celebration
   if (i < 2) {
-    document.getElementById("enc").innerHTML = "It's okay. Try Again!";
+    document.getElementById("comment").innerHTML = "It's okay. Try Again!";
   } else {
-    document.getElementById("enc").innerHTML = "Great Job";
+    document.getElementById("comment").innerHTML = "Great Job";
   }
 }
